c5f3e446b8497bb3a23512f83d9edc2a3468c353
Алексей Мэнн <askozeev@edu.hse.ru>
2022-10-25 05:34:57 +0300

    Initial commit_rest

---
+++ vs_ra/get_cost/models_peewee.py
@@ -0,0 +1,58 @@
+import os
+from decimal import Decimal
+
+from peewee import *
+
+db = SqliteDatabase(f'database1.db', pragmas={'foreign_keys': 1})
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Data(BaseModel):
+    trade_type = CharField(max_length=64)
+    payment = CharField(max_length=64)
+    price = CharField(max_length=64)
+
+
+db.create_tables([Data])
+
+
+def load_data(data):
+    with db.transaction() as txn:
+        for trade_type, payment, price in data:
+            try:
+                data_in_db = Data.select().where((Data.trade_type == trade_type) & (Data.payment == payment))[0]
+                if data_in_db.price != price:
+                    data_in_db.price = price
+                    data_in_db.save()
+            except:
+                Data.create(trade_type=trade_type, payment=payment, price=price)
+        txn.commit()
+
+
+def get_course1(trade_type, payment):
+    for i in Data.select():
+        print(i)
+    a = Data.select().where((Data.trade_type == trade_type) & (Data.payment == payment))
+    print(a)
+    return Decimal(Data.select(Data.price).where((Data.trade_type == trade_type) & (Data.payment == payment))[0].price)
+
+
+def get_course(trade_type, payment):
+    data = [['BUY', 'RosBankNew', '64.97000'], ['BUY', 'TinkoffNew', '65.03000'], ['BUY', 'QIWI', '65.49000'], ['SELL', 'KaspiBank', '472.11000'], ['SELL', 'ForteBank', '470.10000'], ['SELL', 'BankofGeorgia', '2.77000'], ['SELL', 'TBCbank', '2.77000'], ['SELL', 'LIBERTYBANK', '2.65000'], ['SELL', 'Uzcard', '11370.00000'], ['SELL', 'PermataMe', '15594.00000']]
+    for i in data:
+        if i[0] == trade_type and i[1] == payment:
+            return Decimal(i[2])
+    # a = Data.select().where((Data.trade_type == trade_type) & (Data.payment == payment))
+    # print(a)
+    # return Decimal(Data.select(Data.price).where((Data.trade_type == trade_type) & (Data.payment == payment))[0].price)
+
+
+if __name__ == '__main__':
+    temp = []
+    for i in Data.select():
+        temp.append([i.trade_type, i.payment, i.price])
+    print(temp)
