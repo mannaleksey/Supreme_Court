a7a3eacb8f750ea8158b3b12220b5ba6da44950c
Алексей Мэнн <askozeev@edu.hse.ru>
2022-10-26 05:00:54 +0300

    Initial commit

--- vs_ra/get_cost/models_peewee.py
+++ vs_ra/get_cost/models_peewee.py
@@ -41,8 +41,21 @@ def get_course1(trade_type, payment):
     return Decimal(Data.select(Data.price).where((Data.trade_type == trade_type) & (Data.payment == payment))[0].price)
 
 
-def get_course(trade_type, payment):
+def get_course(trade_type, payment_name):
     data = [['BUY', 'RosBankNew', '64.97000'], ['BUY', 'TinkoffNew', '65.03000'], ['BUY', 'QIWI', '65.49000'], ['SELL', 'KaspiBank', '472.11000'], ['SELL', 'ForteBank', '470.10000'], ['SELL', 'BankofGeorgia', '2.77000'], ['SELL', 'TBCbank', '2.77000'], ['SELL', 'LIBERTYBANK', '2.65000'], ['SELL', 'Uzcard', '11370.00000'], ['SELL', 'PermataMe', '15594.00000']]
+    names_bank = {
+        'Сбер (RUB)': 'RosBankNew',
+        'Тинькофф (RUB)': 'TinkoffNew',
+        'QIWI (RUB)': 'QIWI',
+        'Kaspi Bank (KZT)': 'KaspiBank',
+        'Forte Bank (KZT)': 'ForteBank',
+        'Bank of Georgia (GEL)': 'BankofGeorgia',
+        'TBC Bank (GEL)': 'TBCbank',
+        ' Liberty Bank (GEL)': 'LIBERTYBANK',
+        'UZCARD (UZS)': 'Uzcard',
+        'Индонезия (IDR)': 'PermataMe'
+    }
+    payment = names_bank[payment_name]
     for i in data:
         if i[0] == trade_type and i[1] == payment:
             return Decimal(i[2])
@@ -52,7 +65,4 @@ def get_course(trade_type, payment):
 
 
 if __name__ == '__main__':
-    temp = []
-    for i in Data.select():
-        temp.append([i.trade_type, i.payment, i.price])
-    print(temp)
+    pass
