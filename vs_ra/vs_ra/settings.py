a8dd08fb8b1a076a8c5f4514c6d4d249aee66a44
Алексей Мэнн <askozeev@edu.hse.ru>
2022-12-26 08:29:36 +0300

    добавление hearing (пофиксил воспроизведение времени)

--- vs_ra/vs_ra/settings.py
+++ vs_ra/vs_ra/settings.py
@@ -128,10 +128,6 @@ STATIC_ROOT = 'staticfiles/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-TIME_INPUT_FORMATS = [
-    '%H',     # '14:30:59'
-]
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
