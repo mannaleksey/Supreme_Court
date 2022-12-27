72b996d00b44c7835593044b695dc7ebdcc08164
Алексей Мэнн <askozeev@edu.hse.ru>
2022-12-27 22:44:47 +0300

    сделал шапку, как на основном сайте, убрал стадию, подвар отцентровал, убрал лупу

--- vs_ra/main/urls.py
+++ vs_ra/main/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    # path('', RedirectView.as_view(url='https://vs-ra.org/'), name='home'),  # включить при загрузке на сервер
+    path('', RedirectView.as_view(url='https://vs-ra.org/'), name='home'),  # включить при загрузке на сервер
     path('search', views.search, name='search'),
     path('detail', views.detail, name='detail'),
     path('reload/v1/db/every_day', views.reload_search, name='reload'),
